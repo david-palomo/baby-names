8c4cae55e232a152f4b7e3fcbaa119eab718649d
David <davidpm99@gmail.com>
2024-11-26 02:00:04 +0100

    style: adjust name picker styles

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -3,15 +3,11 @@
 </script>
 
 <div in:fly>
-	<div class="flex justify-between">
-		<article class="mb-5 flex space-x-4 py-2">History</article>
-		<article class="mb-5 flex space-x-4 py-2">Partner</article>
-	</div>
-
-	<article class="flex flex-col items-center justify-center py-8 text-center sm:py-10">
-		<p class="pb-4 text-[var(--pico-muted-color)]">Do you like this name?</p>
-		<p class="flex h-24 items-center px-1 pb-6 pt-2 text-xl font-bold xs:text-2xl">John</p>
-		<div class="flex space-x-4">
+	<article class="mb-6 flex h-96 flex-col items-center justify-center text-center">
+		<p class="text-lg text-[var(--pico-muted-color)]">Do you like this name?</p>
+		<p class="flex h-20 items-center text-3xl font-bold">John</p>
+		<button type="button" class="py-1 text-xs font-bold outline">+ info</button>
+		<div class="flex space-x-4 pt-6">
 			<button type="button" class="error-btn w-24 px-4 py-2 text-lg font-bold">no</button>
 			<button type="button" class="w-24 px-4 py-2 text-lg font-bold">yes</button>
 		</div>
