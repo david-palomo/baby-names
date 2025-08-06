c9a1b88cc9ab17c155aa919505869276ec7de126
David <davidpm99@gmail.com>
2025-08-06 21:20:01 +0200

    feat: add error meme, update deps

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
-	import { page } from '$app/state';
+	import peanutBaby from '$lib/assets/peanutbaby.webp';
 	import BackButton from '$lib/components/BackButton.svelte';
 	import { fly } from 'svelte/transition';
 </script>
 
 <BackButton />
 <div in:fly={{ y: 20, duration: 300 }}>
-	<article class="flex flex-col items-center justify-center gap-4 py-10 text-center">
+	<article class="flex flex-col items-center justify-center gap-3 py-10 text-center">
 		<p class="font-title text-3xl font-bold xl:text-4xl">Oh no! 🫢</p>
-		<p class="text-sm text-[var(--pico-muted-color)]">Looks like something went wrong:</p>
-		<p class="text-xl font-bold text-[var(--pico-primary)]">
-			"{page.status} - {page.error?.message}"
-		</p>
-		<p class="max-w-64 text-balance text-sm/6 text-[var(--pico-muted-color)]">
-			The site is under construction, so it might be our fault...
+		<p class="text-[var(--pico-muted-color)]">Looks like something went wrong...</p>
+		<enhanced:img class="rounded" src={peanutBaby} alt="Peanut Butter Baby meme" />
+		<p class="max-w-64 text-balance text-[var(--pico-muted-color)]">
+			The site is under construction, so it might as well be our fault
 		</p>
 	</article>
 </div>
